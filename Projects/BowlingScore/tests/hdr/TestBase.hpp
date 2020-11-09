21a1e4c8ca1976fb1c9ee2301c6ff5a2506ae5e7
Eugene Tolstikhin <etolstikhin@gmail.com>
2020-11-09 15:03:53 +0100

    Added missed part for ConsoleView test [Status] Done

--- Projects/BowlingScore/tests/hdr/TestBase.hpp
+++ Projects/BowlingScore/tests/hdr/TestBase.hpp
@@ -6,7 +6,7 @@
 #include "GameTest.hpp"
 #include "LaneTest.hpp"
 #include "PlayerTest.hpp"
-
+#include "ConsoleViewTest.hpp"
 #include "PointsListenerLocalTest.hpp"
 
 class TestBase : public testing::Test
@@ -29,7 +29,7 @@ public:
 	static TestGame* pGame;
 	static TestLane* pLane;
 	static TestPlayer* pPlayer;
-
+	static TestConsoleView* pConsoleView;
 	static TestPointsListenerLocal* pLocalListener;
 };
 
@@ -40,7 +40,7 @@ TestFrame* TestBase::pFrame = nullptr;
 TestGame* TestBase::pGame = nullptr;
 TestLane* TestBase::pLane = nullptr;
 TestPlayer* TestBase::pPlayer = nullptr;
-
+TestConsoleView* TestBase::pConsoleView = nullptr;
 TestPointsListenerLocal* TestBase::pLocalListener = nullptr;
 
 void TestBase::SetUpTestCase()
@@ -67,8 +67,10 @@ void TestBase::SetUpTestCase()
 		delete pPlayer;
 	}
 
-
-
+	if ( pConsoleView != nullptr )
+	{
+		delete pConsoleView;
+	}
 
 	if ( pLocalListener != nullptr )
 	{
@@ -79,7 +81,7 @@ void TestBase::SetUpTestCase()
 	pGame = new TestGame;
 	pLane = new TestLane;
 	pPlayer = new TestPlayer;
-
+	pConsoleView = new TestConsoleView;
 	pLocalListener = new TestPointsListenerLocal;
 }
 
@@ -91,14 +93,14 @@ void TestBase::TearDownTestCase()
 	delete pGame;
 	delete pLane;
 	delete pPlayer;
-
+	delete pConsoleView;
 	delete pLocalListener;
 
     pFrame = nullptr;
 	pGame = nullptr;
 	pLane = nullptr;
 	pPlayer = nullptr;
-
+	pConsoleView = nullptr;
 	pLocalListener = nullptr;
 }
 #endif // __TESTBASE_H__
