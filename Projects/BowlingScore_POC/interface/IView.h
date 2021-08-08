c2d96f79246f5ed692a5b06a18de18fa44fccc1b
Ievgen Tolstikhin <etolstikhin@gmail.com>
2021-08-08 15:41:58 -0400

    WIP: Last frame calculation

--- Projects/BowlingScore_POC/interface/IView.h
+++ Projects/BowlingScore_POC/interface/IView.h
@@ -20,7 +20,7 @@ public:
 
     virtual void Draw(const ViewElement&, void* params = nullptr) = 0;
     virtual void UpdateFrameScore(const unsigned short, const Flag&) = 0;
-    virtual void UpdateScore(const unsigned short, const short) = 0;
+    virtual void UpdateScore(const unsigned short, const short, bool gameover = false) = 0;
     virtual void CleanScore() = 0;
     virtual void SetNextFrameActive(const bool) = 0;
 };
